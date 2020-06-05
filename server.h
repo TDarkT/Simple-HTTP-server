1ebf6a0d4e5a16a65639b2f26d99c985603dca71
TDarkT <minhlong1312@gmail.com>
2020-06-05 09:42:01 -0700

    Create struct client to pass args to thread

--- server.h
+++ server.h
@@ -1,6 +1,12 @@
 #ifndef SERVER_H
 #define SERVER_H
-
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+struct client {
+    int fd;
+    struct sockaddr_in addr;
+};
 int server_init();
-void *client_connect();
+void *client_connect(void *arg);
 #endif
