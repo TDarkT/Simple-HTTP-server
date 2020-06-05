fe9e2c069c74193742f89439463c29f770d2bb37
TDarkT <minhlong1312@gmail.com>
2020-06-05 09:39:51 -0700

    Remove the accept() to main.c

--- server.c
+++ server.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include "http.h"
 #include "file_lib.h"
-
+#include "server.h"
 #define MAX_CONN 100
 
 extern int signal;
@@ -15,14 +15,14 @@ int opt = 1;
 int server_fd, client_fd[30];
 struct sockaddr_in addr;
 socklen_t ad_length = sizeof(addr);
-int server_init() {
+int server_init(int port) {
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket initialiazation failed\n");
+        fprintf(stderr,"Socket initialiazation failed\n");
         exit(1);
     }
     
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, 4) < 0) {
-        perror("Set socket option falied\n");
+        fprintf(stderr,"Set socket option falied\n");
         exit(1);
     }
     //set the server_fd to NON_BLOCK
@@ -33,38 +33,36 @@ int server_init() {
 
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(8784);
+    addr.sin_port = htons(port);
 
     if (bind(server_fd, (struct sockaddr *)&addr, (socklen_t) sizeof(addr)) < 0) {
-        perror("Can't bind on port 80\n");
+        fprintf(stderr,"Can't bind on port %d\n",port);
         exit(1);
     }
     
     if (listen(server_fd, 0) == -1) {
-        perror("Can't listen on port 80\n");
+        fprintf(stderr,"Can't listen on port %d\n",port);
         exit(1);
     }
     
-    printf("Listening on port 8784...\n");
+    printf("Listening on port %d...\n", port);
     return server_fd;
 }
 
-void* client_connect() {
-    
+void* client_connect(void *arg) {
+
+    struct client *temp = arg;
+    int client_fd = temp->fd;
+    struct sockaddr_in addr = temp->addr;
+
     char buff[32565];
-    //accept 
-    struct sockaddr_in addr;
-    socklen_t addr_length = sizeof(addr);
-    
-    int client_fd = accept(server_fd, (struct sockaddr *)&addr,(&addr_length));
-    signal = 0;
     if (client_fd == -1) {
-        perror("Oops! Something wrong\n");
+        fprintf(stderr,"Oops! Something wrong\n");
     } else {
          // Receive client request 
         if (recv(client_fd, buff, sizeof(buff),0) > 0) {
     	    struct http_header req_h;
-    	    req_h = parse_http_req(buff);
+    	    req_h = parse_http_req(buff,addr);
     	    
             FILE *fp;
     	    char path[80] = ".";
@@ -78,17 +76,14 @@ void* client_connect() {
                 char *header = http_respone_404(req_h, NULL, size);
                 strcat(header, data);
                 send(client_fd, header, strlen(header), 0);
-                free(data);
-                free(header);
+                
             } else {
     	        int size = get_file_size(fp);
                 char *data = get_file_content(fp, size);
                 char *header = http_respone_200(req_h, NULL, size);
     	        strcat(header, data);
 	            send(client_fd, header, strlen(header), 0);
-                free(data);
-                free(header);
-	         }
+             }
     	     shutdown(client_fd, SHUT_RDWR);
     	     close(client_fd); 
         }
